09e32e7e7c90e66b6928723e3058e5091a7db326
Pricetx <pricetx@hotmail.com>
2013-01-09 11:29:07 +0000

    Update ggo.sh
    Removed debug flag.

--- ggo.sh
+++ ggo.sh
@@ -91,7 +91,7 @@ PID=$(pgrep -u $(whoami) -f "session=${NAME}" )
 INFO="${GAME} screen session '${NAME}'"
 
 SCREENCMD="screen -dmS ${NAME}"
-ARGS="-Dsession=${NAME} -console -game ${GAME} -port ${PORT} +map ${MAP} -maxplayers ${MAXPLAYERS} -debug"
+ARGS="-Dsession=${NAME} -console -game ${GAME} -port ${PORT} +map ${MAP} -maxplayers ${MAXPLAYERS}"
 STARTCMD="${SCREENCMD} ${BIN} ${ARGS}"
 
 
