0166cd55472f51c57c76009dd620b25ed6400c1f
Jonathan Price <pricetx@hotmail.com>
2013-04-29 16:59:50 +0200

    Update ggo.sh
    Various changes to bring it inline with local copy. Removed some unused sections such as advert()

--- ggo.sh
+++ ggo.sh
@@ -41,13 +41,19 @@ IP=""
 ####    SYSTEM SETTINGS    ####
 #### EDIT THESE PARAMETERS ####
 
+# Set this to the AppID for the game you're using
+# List can be found here: https://developer.valvesoftware.com/wiki/Steam_Application_IDs
+APPID="232370"
 
 # Set this to the location of your orangebox folder.
-DIR="/home/ggo/srcds/orangebox"
+DIR="/home/ggo/gravitygunonly"
 
 # Set this to the location to the folder up from your orangebox
 # folder, i.e the folder that contains hldsupdatetool.bin
-UPDATEDIR="/home/ggo/srcds"
+UPDATEDIR="/home/ggo/steamcmd"
+
+# Set this to the RELATIVE path for the installation, from the UPDATEDIR
+RELATIVEUPDATEDIR="../gravitygunonly/"
 
 # This variable sets the name of the GNU Screen session.
 # Make sure that this is NOT the same as your UNIX username.
@@ -59,7 +65,7 @@ BIN="./srcds_run"
 
 # Set this to the location of your steam binary.
 # This should NOT usually be changed.
-UPDATEBIN="./steam"
+UPDATEBIN="./steam.sh"
 
 # If you get a message  on startup asking you to set the RSTDC_FREQUENCY
 # Then set that value here, otherwise, leave the field blank.
@@ -73,29 +79,6 @@ DEMODIR="/home/ggo/web/hl2mp/demos/"
 # in minutes you want to store demo files on your web server, e.g 10080 
 # is 1 week.
 DEMOAGE="10080"
-
-
-######################################################################
-
-
-####   LIST OF FILES TO RESTORE  ####
-#### TO AVOID UPDATE OVERWRITING ####
-
-# Set this to the location you want to back up files to, so as not
-# to get overwritten by an update
-BACKUPDIR="/home/ggo/srcds/updatefiles"
-
-# Name of the file (as seen in $BACKUPDIR)
-BACKUP_1="game_sounds_manifest.txt"
-# Full path to the file
-BACKUPLOCATION_1="/home/ggo/srcds/orangebox/hl2/scripts/game_sounds_manifest.txt"
-
-BACKUP_2="maplist.txt"
-BACKUPLOCATION_2="/home/ggo/srcds/orangebox/hl2mp/maplist.txt"
-
-BACKUP_3="valve.rc"
-BACKUPLOCATION_3="/home/ggo/srcds/orangebox/hl2mp/cfg/valve.rc"
-
 
 #######################################################################
 
@@ -120,7 +103,7 @@ STARTCMD="${SCREENCMD} ${BIN} ${ARGS}"
 ####     UPDATE PARAMETERS    ####
 #### DO NOT EDIT THIS SECTION ####
 
-UPDATEARGS="-command update -game ${GAME} -dir ."
+UPDATEARGS="+login anonymous +force_install_dir ${RELATIVEUPDATEDIR} +app_update \"${APPID} -beta prerelease\" validate +quit"
 UPDATECMD="${SCREENCMD} ${UPDATEBIN} ${UPDATEARGS}"
 
 
@@ -179,7 +162,7 @@ updateGGORunning() {
 	echo "Giving 10 second countdown warning."
 	for i in {10..1}
         do
-                echo -n "$i "
+        	echo -n "$i "
                 sendCommand "say The server is updating in $i seconds."
                 sleep 1
         done
@@ -195,14 +178,6 @@ updateGGORunning() {
         while [[ `screen -ls |grep $NAME` ]]; do
                 sleep 1
         done
-	cd "$BACKUPDIR"
-	echo "Copying '${BACKUP_1}' back"
-	cp ${BACKUP_1} ${BACKUPLOCATION_1}
-	echo "Copying '${BACKUP_2}' back"
-	cp ${BACKUP_2} ${BACKUPLOCATION_2}
-	echo "Copying '${BACKUP_3}' back"
-	cp ${BACKUP_3} ${BACKUPLOCATION_3}
-	sleep 2
 	cd "$DIR"
 	startGGO
 }
@@ -217,30 +192,6 @@ updateGGONotRunning() {
                 sleep 1
         done
 	echo "Update complete."
-}
-
-advert() {
-        echo "Starting Advertes."
-        echo "NOTE: This script takes 30 minutes to run. It is recommended to run this as a cron job."
-        sendCommand "say [NOTICE] Visit our website: www.gravitygunonly.com"
-        sleep 180
-        sendCommand "say [NOTICE] Bored of this map? type rtv in the chatbox."
-        sleep 180
-        sendCommand "say [NOTICE] The admins are: Pricetx, A-Lizzard-With-A-Fez"
-        sleep 180
-        sendCommand "say [NOTICE] View our stats page at www.gravitygunonly.com"
-        sleep 180
-        sendCommand "say [NOTICE] Suggest a map at www.gravitygunonly.com"
-        sleep 180
-        sendCommand "say [NOTICE] type motd to see a list of helpful commands."
-        sleep 180
-        sendCommand "say [NOTICE] View our forum thread at www.gravitygunonly.com"
-        sleep 180
-        sendCommand "say [NOTICE] type top10 to see the leaderboards."
-        sleep 180
-        sendCommand "say [NOTICE] Visit www.gravitygunonly.com for demo files of every match."
-        sleep 180
-        sendCommand "say [NOTICE] type motd to see the Message of the Day."
 }
 
 demo() {
