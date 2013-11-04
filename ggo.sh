fc525304c84732d64665637cd98e4ee0cedd4beb
Jonathan Price <pricetx@hotmail.com>
2013-11-04 23:14:39 +0000

    Update ggo.sh
    Complete re-write using bourne shell and tmux. Works, but somewhat fragile currently.

--- ggo.sh
+++ ggo.sh
@@ -1,241 +1,262 @@
-#!/usr/bin/env bash
-#####################################################################
-# Gravity Gun Only bash script                                      #
-# developed by Pricetx.                                             #
-#####################################################################
+#!/bin/sh
+#-
+#
+# Copyright (c) 2013 Jonathan Price <jonathan@jonathanprice.org>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
 
-####    INGAME SETTINGS    ####
-#### EDIT THESE PARAMETERS ####
+# GGO
+#
+# Server managing script for SteamCMD servers, using tmux. Includes commands
+# to start, stop and restart the server, as well as being able to update the
+# server, and handle transferring SourceTV demo files to a web directory.
+#
+# Comments, suggestions, bug reports please to:
+# Jonathan Price <jonathan@jonathanprice.org>
 
-# The game's name, as srcds calls it, e.g hl2mp
+# The SteamCMD name for the game. A list of names can be found here:
+# https://developer.valvesoftware.com/wiki/Game_Name_Abbreviations
 GAME="hl2mp"
 
-# Set this to the AppID for the game you're using
-# List can be found here: https://developer.valvesoftware.com/wiki/Steam_Application_IDs
+# The AppID of the game you're using. A list can be found here:
+# https://developer.valvesoftware.com/wiki/Steam_Application_IDs
 APPID="232370"
 
-# Set this to the map you want the server to load on launch.
+# The map to load on launch.
 MAP="dm_overwatch"
 
-# Set this to the maximum number of slots you want the server to have.
+# The number of player slots.
 MAXPLAYERS="16"
 
-# Set this to the port that the server will run on.
+# The port to bind to
 PORT="27015"
 
-# If you have multiple IPs and want to bind to one, enter it here.
-# Otherwise, leave blank.
+# The IP to bind to. Leave blank for all IPs
 IP=""
 
-# Set this to the location of your game folder
+# location of the game folder
 DIR="/home/ggo/gravitygunonly"
 
-# Set this to your SteamCMD folder (the one that contains steamcmd.sh)
-UPDATEDIR="/home/ggo/steamcmd"
+# The location of your SteamCMD binary
+UPDATEDIR="/home/ggo/steamcmd/"
 
-# This variable sets the name of the GNU Screen session.
-# Make sure that this is NOT the same as your UNIX username.
+# The name given to the tmux session
 NAME="gravitygunonly"
 
-# *OPTIONAL* If you have a web server on your server, enter a directory
-# To store demo files for public download.
+# OPTIONAL: If you have a web server, enter a directory here to store
+# demo files
 DEMODIR="/home/ggo/web/hl2mp/demos/"
 
-# *OPTIONAL* If you are using a web server as shown above, enter the age
-# in minutes you want to store demo files on your web server, e.g 10080
-# is 1 week.
+# OPTIONAL: If you have a web server, set the time in minutes to save
+# demo files for. 10080 is 1 week.
 DEMOAGE="10080"
 
-# If you get a message  on startup asking you to set the RDTSC_FREQUENCY
-# Then set that value here, otherwise, leave the field blank.
+# OPTIONAL: If you are asked to set "RDTSC_FREQUENCY" on startup, enter
+# the value here. Otherwise, leave it blank.
 RDTSC=""
 
 
-######################################################################
+
+# STARTUP PARAMETERS
+# DO NOT EDIT THIS SECTION
 
 
-####    STARTUP PARAMETERS    ####
-#### DO NOT EDIT THIS SECTION ####
 
-# Gathers a list of all process IDs relating to the server.
-PID=$(pgrep -u $(whoami) -f "session=${NAME}" )
+# Determines whether there is a tmux server running
+TMUXPID=$(pgrep -u "$(whoami)" -f "tmux: server" )
 
-# Prints out the name of the running server.
-INFO="${GAME} screen session '${NAME}'"
+# Prints the game and tmux session
+INFO="Game: $GAME | tmux session: $NAME"
 
-SCREENCMD="screen -mS ${NAME}"
+# The name of the main binary
+BIN="srcds_run"
+
+# The name of the main SteamCMD script file
+UPDATEBIN="steamcmd.sh"
+
+# Server start arguments
 ARGS="-console -game ${GAME} -port ${PORT} -maxplayers ${MAXPLAYERS} -ip ${IP} +map ${MAP}"
-STARTCMD="${SCREENCMD} ./srcds_run ${ARGS}"
 
+# Server update arguments
 UPDATEARGS="+login anonymous +force_install_dir ${DIR} +app_update ${APPID} -beta prerelease validate +quit"
-UPDATECMD="${SCREENCMD} ./steamcmd.sh ${UPDATEARGS}"
+
+# Is "stopping" or "restarting" based on command given
+STATUS=""
 
 
-#######################################################################
+
+# FUNCTIONS
+# DO NOT EDIT THIS SECTION
 
 
-####      SCRIPT ACTIONS      ####
-#### DO NOT EDIT THIS SECTION ####
 
-# the PATH line is only needed for if you run demo as a cronjob
-# and even then it might just be FreeBSD that needs this.
-PATH="/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin"
+# Starts the server, performing the following pre-startup checks:
+# is tmux installed
+# Does a tmux session with the same name already exist
+# does the game folder $DIR exist
+# has an $RDTSC value been given
+doStart() {
+	if [ ! "$(command -v tmux)" ]; then
+		echo "ERROR: tmux could not be found, is it installed?"
+		exit 1
+	fi
 
-startServer() {
-	cd "$DIR"
-  	export RDTSC_FREQUENCY='${RDTSC}'
-	echo "Starting Server."
-	${STARTCMD}
-}
-
-stopServer() {
-	cd "$DIR"
-	echo "Giving 10 second countdown warning."
-	for i in {10..1}
-	do
-		echo -n "$i "
-    	sendCommand "say The server is stopping in $i seconds."
-        sleep 1
-    done
-	echo "Stopping Server."
-	sendCommand quit
-    while [[ `screen -ls |grep $NAME` ]]; do
-      	sleep 1
-    done
-	echo "Server stopped."
-}
-
-restartServer() {
-	cd "$DIR"
-    echo "Giving 10 second countdown warning."
-	for i in {10..1}
-	do
-		echo -n "$i "
-		sendCommand "say The server is restarting in $i seconds."
-		sleep 1
-	done
-    echo "Stopping Server."
-	sendCommand quit
-	while [[ `screen -ls |grep $NAME` ]]; do
-		sleep 1
-	done
-	sleep 2
-	startServer
-}
-
-updateServerRunning() {
-	cd "$UPDATEDIR"
-	echo "Giving 10 second countdown warning."
-	for i in {10..1}
-    do
-    	echo -n "$i "
-    	sendCommand "say The server is updating in $i seconds."
-    	sleep 1
-    done
-	echo "Stopping Server."
-	sendCommand quit
-    while [[ `screen -ls |grep $NAME` ]]; do
-    	sleep 1
-    done
-	sleep 2
-	${UPDATECMD}
-    while [[ `screen -ls |grep $NAME` ]]; do
-        sleep 1
-    done
-	cd "$DIR"
-	startServer
-}
-
-updateServerNotRunning() {
-	echo "Updating Server."
-	cd $UPDATEDIR
-	${UPDATECMD}
-    while [[ `screen -ls |grep $NAME` ]]; do
-        sleep 1
-    done
-	echo "Update complete."
-	cd "$DIR"
-}
-
-demo() {
-	echo "Moving old demos to web server."
-	#Moves all .dem files to the web server if they're older than 2 hours (to avoid cutting a demo short)
-	find ${DIR}/${GAME}/ -iname "*.dem" -mmin +120 -exec chmod 664 {} \; -exec mv {} ${DEMODIR} \;
-
-	#Delete all archived demo files older than period specified
-	find ${DEMODIR} -name "*.dem" -mmin +${DEMOAGE} -exec rm {} \;
-	echo "Move complete."
-}
-
-sendCommand() {
-	local command=$(printf "$@\r")
-	sh -c "screen -p0 -S "${NAME}" -X stuff \"${command}\""
-}
-
-####################################################################
-
-case "$1" in
-	start)
-		if [[ ! `screen -ls |grep $NAME` ]]; then
-			if [[ `type screen` ]]; then
-				if [[ -e $DIR ]]; then
-					if [[ -e $DIR/$GAME/maps/$MAP.bsp ]]; then
-						echo "Starting ${INFO}"
-						startServer
-					else
-						printf "$MAP is not in the maps folder, please choose a new map\n"
-					fi
-				else
-					printf "${DIR} does not exist, please edit the DIR variable in the script\n"
-				fi
-			else
-				printf "GNU Screen is not installed, please install it before continuing\n"
-			fi
-		else
-            printf "Screen name ${NAME} is already in use\n"
-            printf "Please either stop it or choose a new name\n"
+	if [ "$TMUXPID" ]; then
+		if [ -n "$(tmux list-sessions | grep $NAME)" ]; then
+			echo "ERROR: A tmux session with the name $NAME already exists"
+			exit 1
 		fi
+	fi
+
+	if [ ! -d $DIR ]; then
+		echo "ERROR: Could not find $DIR. Is the path correct?"
+		exit 1
+	fi
+
+	if [ $RDTSC ]; then
+		export RDTSC_FREQUENCY=$RDTSC
+		echo "DEBUG: an RDTSC value has been provided"
+	fi
+
+	echo "Starting server"
+	tmux new-session -d -s $NAME "$DIR/$BIN $ARGS"
+	tmux attach-session -t $NAME
+}
+
+
+# Stops the server, performing the following checks first
+# Checks if tmux is running
+# Checks if a tmux session with the correct name exists
+doStop() {
+    if [ ! "$TMUXPID" ]; then
+		echo "ERROR: tmux is not running"
+		exit 1
+    fi
+
+	if [ ! -n "$(tmux list-sessions | grep $NAME)" ]; then
+		echo "ERROR: A tmux session with the name $NAME couldn't be found"
+		exit 1
+	fi
+
+	echo "Giving 10 second countdown warning."
+
+	for i in 10 9 8 7 6 5 4 3 2 1 ; do
+		printf "%s " "$i"
+		tmux send-keys -t $NAME "say The server is $STATUS in $i seconds." ENTER
+		sleep 1
+	done
+	echo "Stopping Server"
+	tmux send-keys -t $NAME "quit" ENTER
+
+    while [ -n "$(tmux list-sessions | grep $NAME)" ]; do
+		sleep 1
+	done
+}
+
+# Restarts the server. For checks, see stop and start functions.
+doRestart() {
+	doStop
+	doStart
+}
+
+# Updates the server. Before doing so, it runs the following checks:
+# Checks to see if the server is running. If so, stop the server,
+# and start it again aftwards.
+doUpdate() {
+	local wasRunning="false"
+    if [ -n "$(tmux list-sessions | grep $NAME)" ]; then
+        echo "$NAME is currently running. Stopping before updating."
+		wasRunning="true"
+		doStop
+    fi
+
+	echo "Updating server"
+	tmux new-session -d -s $NAME "$UPDATEDIR/$UPDATEBIN $UPDATEARGS"
+	tmux attach-session -t $NAME
+    while [ -n "$(tmux list-sessions | grep $NAME)" ]; do
+        sleep 1
+    done
+	if [ $wasRunning = "true" ]; then
+		doStart
+		echo lets all start!
+	fi
+}
+
+doDemo() {
+    echo "Moving old demos to web server."
+    #Moves all .dem files to the web server if they're older than 2 hours (to avoid cutting a demo short)
+    find ${DIR}/${GAME}/ -iname "*.dem" -mmin +120 -exec chmod 664 {} \; -exec mv {} ${DEMODIR} \;
+
+    #Delete all archived demo files older than period specified
+    find ${DEMODIR} -name "*.dem" -mmin +${DEMOAGE} -exec rm {} \;
+    echo "Move complete."
+}
+
+doConsole() {
+        tmux attach-session -t $NAME
+}
+
+doStatus() {
+	echo "$INFO"
+	if [ -n "$(tmux list-sessions | grep $NAME)" ]; then
+		echo "Status: running"
+	else
+		echo "Status: not running"
+	fi
+}
+
+# COMMAND INTEPRETER
+# DO NOT EDIT THIS SECTION
+
+
+
+case $1 in
+	start)
+		doStart
 	;;
 	stop)
-		if [[ `screen -ls |grep $NAME` ]]; then
-			stopServer
-		else
-        	printf "${INFO} is not running, not stopping\n"
-		fi
+		STATUS="stopping"
+		doStop "$2"
 	;;
 	restart)
-        if [[ `screen -ls |grep $NAME` ]]; then
-			restartServer
-		else
-			printf "${INFO} is not running. Not restarting.\n"
-		fi
+		STATUS="restarting"
+		doRestart
 	;;
 	update)
-        if [[ `type screen` ]]; then
-			if [[ -e $UPDATEDIR ]]; then
-				if [[ `screen -ls |grep $NAME` ]]; then
-       				printf "${INFO} is running. Warning, then stopping to update.\n"
-           			updateServerRunning
-				else
-		            printf "${INFO} is not running. Updating anyway.\n"
-		            updateServerNotRunning
-				fi
-			else
-				printf "$UPDATEDIR doesn't exist. Please edit UPDATEDIR in the script\n"
-			fi
-		else
-			printf "GNU Screen is not installed, please install it before continuing\n"
-		fi
+		doUpdate
 	;;
 	demo)
-		if [[ -e $DEMODIR ]]; then
-			demo
-		else
-			printf "$DEMODIR does not exist, please edit DEMODIR in the script"
-		fi
+		doDemo
+	;;
+	console)
+		doConsole
+	;;
+	status)
+		doStatus
 	;;
 	*)
-		printf "Usage: $0 {start|stop|restart|update|demo}\n"
+		printf "%s" "Usage: $0 {start|stop|restart|update|demo|console|status}\n"
 		exit 2
 	;;
 esac
