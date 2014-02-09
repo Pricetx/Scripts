15bad0a4dae8d326b4b79b4b8b8f88ef9e07ba64
Jonathan Price <pricetx@hotmail.com>
2014-02-09 11:42:12 +0000

    Update steamcmd.sh
    Updated base SteamCMD script to latest version.

--- steamcmd.sh
+++ steamcmd.sh
@@ -1,23 +1,35 @@
-#!/usr/local/bin/bash
-# SteamCMD script for FreeBSD
+#!/usr/bin/env bash
 
-STEAMROOT=$(cd "${0%/*}" && echo $PWD)
-STEAMEXE=steamcmd
+STEAMROOT="$(cd "${0%/*}" && echo $PWD)"
+STEAMEXE="steamcmd"
+
+UNAME=`uname`
 PLATFORM=linux32
-export LD_LIBRARY_PATH="${STEAMROOT}"/${PLATFORM}:$LD_LIBRARY_PATH
-PATHLINK=~/.steampath
-rm -f ${PATHLINK} && ln -s ${STEAMROOT}/${PLATFORM}/${STEAMEXE} 
-${PATHLINK}
-PIDFILE=~/.steampid
-echo $$ > ~/.steampid
+export LD_LIBRARY_PATH="$STEAMROOT/$PLATFORM:$LD_LIBRARY_PATH"
 
 ulimit -n 2048
 
-# and launch steam
-cd "$STEAMROOT"
-STATUS=42
-while [ $STATUS -eq 42 ]; do
-${DEBUGGER} "${STEAMROOT}"/${PLATFORM}/${STEAMEXE} "$@"
+MAGIC_RESTART_EXITCODE=42
+
+if [ "$DEBUGGER" == "gdb" ] || [ "$DEBUGGER" == "cgdb" ]; then
+    ARGSFILE=$(mktemp $USER.steam.gdb.XXXX)
+
+    # Set the LD_PRELOAD varname in the debugger, and unset the global version.
+    if [ "$LD_PRELOAD" ]; then
+        echo set env LD_PRELOAD=$LD_PRELOAD >> "$ARGSFILE"
+        echo show env LD_PRELOAD >> "$ARGSFILE"
+        unset LD_PRELOAD
+    fi
+
+    $DEBUGGER -x "$ARGSFILE" "$STEAMROOT/$PLATFORM/$STEAMEXE" "$@"
+    rm "$ARGSFILE"
+else
+    $DEBUGGER "$STEAMROOT/$PLATFORM/$STEAMEXE" "$@"
+fi
+
 STATUS=$?
-done
+
+if [ $STATUS -eq $MAGIC_RESTART_EXITCODE ]; then
+    exec "$0" "$@"
+fi
 exit $STATUS
